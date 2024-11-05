beb44fef81b197ec22c4dd8d50644ede46acacb5
SaadiaEl08 <saadiaelachguir@gmail.com>
2024-11-05 13:57:08 +0100

    adding sparkles

--- src/components/RotatingDodecahedron.jsx
+++ src/components/RotatingDodecahedron.jsx
@@ -1,17 +1,19 @@
+import { Sparkles } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 function RotatingDodecahedron() {
     const meshRef = useRef();
     useFrame(() => {
       if (meshRef.current) {
-        meshRef.current.rotation.x += 0.01;
-        meshRef.current.rotation.y += 0.01;
+        meshRef.current.rotation.x += 0.001;
+        meshRef.current.rotation.y += 0.001;
       }
     });
     return (
       <mesh ref={meshRef}>
         <dodecahedronGeometry args={[1,0]} />
         <meshLambertMaterial color="#468585" emissive="#468585" />
+        <Sparkles count={200} scale={10} size={7}  speed={0.02} noise={0.002} color="yellow"/>
       </mesh>
     );
   }
